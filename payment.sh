9c0fc19bf86d87b3da41e7b19445d8651c1e0b59
amara <rama@gmail.com>
2025-10-11 14:17:53 +0530

    shipping.sh

---
+++ payment.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.kaws86s.shop
+START_TIME=$(date +%s)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)" | tee -a $LOG_FILE
+
+ USERID=$(id -u)
+if [ $USERID -ne 0 ];then
+    echo  "ERROR:: please run script with root user privilege"
+    exit 1
+fi
+
+VALIDATE(){ #functions recieve inputs through orgs just like shell script
+    if [ $1 -ne 0 ];then
+        echo -e "  $2...$R Failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "  $2...$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ];then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "user already exist... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading payment application"
+
+cd /app &>>$LOG_FILE
+VALIDATE $? "changing app directory"
+
+rm -rf /app/* &>>$LOG_FILE
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/shipping.zip  &>>$LOG_FILE
+VALIDATE $? "Unzip shipping"
+
+pip3 install -r requirements.txt &>>$LOG_FILE
+VALIDATE $? "installing dependencies"
+
+systemctl daemon-reload
+systemctl enable payment &>>$LOG_FILE
+VALIDATE $? "Enabling payment"
+
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "starting payment"
